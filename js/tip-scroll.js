94cb1eb55b827beea2421871a19937f502c52abf
UvealSnow <kevhah92@gmail.com>
2016-07-08 14:19:32 -0500

    fixed UI bug

--- js/tip-scroll.js
+++ js/tip-scroll.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
+	// $('.bg').imagefill();
 	$('.img-container').imagefill();
+	$('#fullpage').fullpage({
+		anchors: ['cover','firstPage', 'secondPage', '3rdPage'],
+	});
 });
