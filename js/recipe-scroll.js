77130c656ffe6d30421b871e0c6a94fcd774a9b9
elujambio <eugeniolujambio@gmail.com>
2016-07-13 14:08:27 -0500

    Recipes style done, article scroll almost there

--- js/recipe-scroll.js
+++ js/recipe-scroll.js
@@ -1,57 +1,92 @@
+var adjustDeltaY = 0;
+var currentDeltaY = null;
 
 function introSwipeUp(ev) {
-	$(".recipe")
-		.velocity("scroll", 
-			{
-				offset:0, 
-				duration:450
-			});
+	$(".recipe-intro")
+		.velocity(
+				{ translateY:"-100%", translateZ: 0} ,
+				{ duration: 250, easing: "linear", complete: function(){
+					$(".recipe")
+						.css("position", "relative");
+				} }
+			); 
 }
+
 function recipeSwipeDown(ev) {
 	console.log("swipe down: " + ev.gesture.velocityY);
 	console.log("r scroll top: " + ($(".recipe").offset().top - $(window).scrollTop()));
-	if( ev.gesture.velocity > 1.4 ) {
-		// console.log("lalalala");
-		$(".recipe")
-			.velocity("scroll", {
-				offset: 0,
-				duration:250
-			});
-	}
-	if( ($(".recipe").offset().top - $(window).scrollTop()) == 0 ) {
+	if ( ($(".recipe").offset().top - $(window).scrollTop()) == 0 && ev.gesture.velocityY > 1.2) {
 		$(".recipe-intro")
-			.velocity("scroll", {
-				offset:0, duration:250
-			});
+			.velocity(
+					{ translateY:"0%", translateZ: 0} ,
+					{ duration: 250, easing: "linear", complete: function(){
+						$(".recipe")
+							.css("position", "fixed");
+					} }
+				); 
+		
 	}
+	// if( ev.gesture.velocity > 1.4) {
+	// 	// console.log("lalalala");
+	// 	$(".recipe")
+	// 		.velocity("scroll", {
+	// 			offset: 0,
+	// 			duration:250
+	// 		});
+	// }
+	// if( ($(".recipe").offset().top - $(window).scrollTop()) == 0 ) {
+	// 	$(".recipe-intro")
+	// 		.velocity("scroll", {
+	// 			offset:0, duration:250
+	// 		});
+	// }
+}
+function recipePanDown(ev) {
+	if(ev.gesture.velocityY < 1.2) {
+		$(".recipe-intro")
+			.velocity(
+					{ translateY: "+=" + (300/ev.gesture.deltaY) },
+					{ duration: 0 }
+				);
+	}
+}
+function recipePanDownEnd(ev) {
+	if(ev.gesture.velocityY < 1.2) {
+		$(".recipe-intro").velocity("stop");
+
+		$(".recipe-intro")
+			.velocity(
+				{ translateY: "-100%" },
+				{ duration:200 }
+				);
+	}
+
 }
 function recipeSwipeUp(ev) {
 	console.log("swipe up: " + ev.gesture.velocityY);
-	if( ev.gesture.velocityY >= 10 ) {
-		$(".recipe")
-			.velocity("scroll", {
-				offset:0, duration:100
-			});
-	}
-
+}
+function recipePanStart(ev) {
 }
 function recipePan(ev) {
-	console.log("pan");
+}
+function recipePanUp(ev) {
 
 }
+
+
+
 $(document).ready(function() {
 
 	$(".recipe-intro")
 		.hammer({
 			recognizers: [
-				[Hammer.Swipe,{direction: Hammer.DIRECTION_ALL}],
-				[Hammer.Pan,{direction: Hammer.DIRECTION_VERTICAL}, ['swipe']]
+				[Hammer.Swipe,{direction: Hammer.DIRECTION_ALL}]
 			]
 		})
 		.bind("swipeup", introSwipeUp);
 	$(".recipe")
 		.hammer({
-			// touchAction: "pan-y",
+			touchAction: "pan-y",
 			recognizers: [
 				[Hammer.Swipe,{direction: Hammer.DIRECTION_ALL}],
 				[Hammer.Pan,{direction: Hammer.DIRECTION_VERTICAL}, ['swipe']]
@@ -59,6 +94,6 @@ $(document).ready(function() {
 		})
 		.bind("swipeup", recipeSwipeUp)
 		.bind("swipedown", recipeSwipeDown)
-		.bind("pan", recipePan);
-
+		.bind("pandown", recipePanDown)
+		.bind("panend", recipePanDownEnd)
 });
