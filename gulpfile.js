213eeb471f02a8ebc15ca5386aed28b617c0bcf2
elujambio <eugeniolujambio@gmail.com>
2016-07-25 11:20:10 -0500

    Added gulp task smushit (image optimizer), optimized all images, and removed mock.jpg (32mb filesize)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var smushit = require('gulp-smushit');
  
-gulp.task('default', function () {
+gulp.task('smushit', function () {
     return gulp.src('img/**/*.{jpg,png}')
         .pipe(smushit())
         .pipe(gulp.dest('dist'));
